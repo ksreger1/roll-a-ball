44fdc5815e8307c090ec396d4acc4ba283f8f249
ksreger1 <43304349+ksreger1@users.noreply.github.com>
2018-10-28 15:44:24 -0500

    Add interactivity
    Add objects to collect and count those objects

---
+++ Assets/Scripts/Coin_FloatSpin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin_FloatSpin : MonoBehaviour {
+	
+	void Start () {
+		float offset = Random.value;
+		
+		transform.Rotate(new Vector3(0, 0, 45) * Random.value);
+		transform.Translate(0.01f * Vector3.up * Mathf.Cos(Random.value * 3f), Space.World);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+		transform.Translate(0.01f * Vector3.up * Mathf.Cos(Time.fixedTime * 3f), Space.World);
+	}
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour 
+{
+	public float speed;
+	public Text countText;
+	public Text victoryText;
+	
+	private Rigidbody _rb;	
+	private int _count;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+	{
+		_rb = GetComponent<Rigidbody>();
+		_count = 0;
+		SetCountText();
+		victoryText.text = "";
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () 
+	{
+		float horizontalMovement = Input.GetAxis ("Horizontal");
+		float verticalMovement = Input.GetAxis ("Vertical");
 		
+		Vector3 movement = new Vector3(horizontalMovement, 0.0f, verticalMovement);
+		_rb.AddForce(movement * speed);
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.CompareTag("Coin"))
+		{
+			other.gameObject.SetActive(false);
+			_count++;
+			SetCountText();
+		}
+	}
+	
+	void SetCountText()
+	{
+		countText.text = string.Format("Count: {0}", _count.ToString());
+		
+		if(_count >= 12)
+		{
+			victoryText.text = "You win!";
+		}
 	}
 }
